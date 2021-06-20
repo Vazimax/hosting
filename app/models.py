9586261c691cc0d0783c54c4416d950ce2d2074f
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-06-20 19:00:47 +0100

    first commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
